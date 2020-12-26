0a56abcdb1a340462274bffb42502c65a5b3951a
Roman Munar <roman.munar1@gmail.com>
2020-12-26 06:16:08 +0000

    changed js files to tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css"
+import "tailwindcss/tailwind.css"
+import { AppProps } from "next/app"
+import { ReactNode } from "react"
+
+const Noop: ReactNode = ({ children }: { children: ReactNode }) => <>{children}</>
+function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = (Component as any).Layout || Noop
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
