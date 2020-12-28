634e73d647ff2d7805d05a334eefb33ebc2c4748
roman.munar <roman.munar1@gmail.com>
2020-12-28 19:14:24 +0800

    added extra colors and height utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amber: colors.amber,
+        gray: colors.warmGray,
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      zIndex: {
+        1: "1",
+      },
+      minHeight: {
+        page: "calc(100vh - 68px)",
+      },
+      maxHeight: {
+        page: "calc(100vh - 68px)",
+      },
+    },
   },
   variants: {
     extend: {},
