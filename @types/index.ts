c7f7435fee1e0cd1dbc175da8d4f761378ed13f2
romanmunar <roman.munar1@gmail.com>
2020-12-28 12:22:41 +0800

    added  property interface

---
+++ @types/index.ts
@@ -0,0 +1,20 @@
+export interface Property {
+  title: string
+  location: string
+  description: string
+  price: number
+  price_min: number
+  price_max: number
+  bedrooms_count: number
+  bathrooms_count: number
+  images: any
+  has_garden: boolean
+  pets_allowed: boolean
+  has_garage: boolean
+  garages_count: number
+  lot_area: number
+  floor_area: number
+  owner_name: string
+  owner_email: string
+  owner_phone_number: string
+}
