55be7ec32b8f6ee998db05d5f9e5f2c272f5481a
roman.munar <roman.munar1@gmail.com>
2020-12-28 20:11:02 +0800

    added home image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import { Layout } from "../components"
-
+import Image from "next/image"
 const Home = () => {
-  return <div className="font-serif max-h-page">hello</div>
+  return (
+    <div className="w-full font-serif min-h-page">
+      <div className="relative">
+        <div
+          style={{ backgroundImage: 'url("/homepace_image.jpg")' }}
+          className="bg-no-repeat bg-cover w-fullbg-center h-52"
+        ></div>
+        <div className="absolute top-0 w-full h-full bg-gray-700 bg-opacity-50"></div>
+        <div className="absolute top-0 z-10 text-white">
+          <span className="text-amber-400">John Hitchkins</span> is a Real Estate Partners is #1 in the Santa Barbara
+          MLS for transactions with over $1 Billion in sales.
+        </div>
+      </div>
+    </div>
+  )
 }
 Home.Layout = Layout
 export default Home
