c1cdb861d1ab18dc4751c0e24fd1e86b6bcd3a47
romanmunar <roman.munar1@gmail.com>
2020-12-29 20:20:57 +0800

    fixed menu's positioning

--- components/core/navbar.tsx
+++ components/core/navbar.tsx
@@ -7,19 +7,19 @@ const Navbar = (props: Props) => {
   const [isOverlayOpen, setIsOverlayOpen] = useState(false)
 
   return (
-    <nav className="flex items-center justify-between px-3 py-2">
+    <nav className="sticky top-0 z-20 flex items-center justify-between px-3 py-2 bg-gray-800">
       <div className="flex items-center">
-        <Logo className="w-10 h-10" />
+        <Logo className="w-8 h-8 text-white" />
         <span className="text-2xl font-bold">
-          <span className="text-amber-400">H</span>
-          <span className="text-gray-700">itchkins</span>
+          <span className="text-amber-500 text-md">H</span>
+          <span className="text-sm text-gray-200">itchkins</span>
         </span>
       </div>
       <div className="h-full sm:hidden">
         {isOverlayOpen && (
           <button
             onClick={() => setIsOverlayOpen(false)}
-            className="absolute inset-0 w-full h-full bg-gray-500 bg-opacity-30 sm:block"
+            className="absolute inset-0 z-40 w-full h-full bg-gray-500 bg-opacity-30 sm:block"
           >
             <div className="relative w-full h-1/2">
               <ul className="absolute flex flex-col items-center justify-around w-full h-full py-10 space-y-4 text-white bg-gray-700 rounded-tl-2xl rounded-tr-2xl top-1/2">
@@ -32,7 +32,7 @@ const Navbar = (props: Props) => {
           </button>
         )}
         <button onClick={() => setIsOverlayOpen(true)}>
-          <Menu className="w-6 h-6" />
+          <Menu className="w-6 h-6 text-white" />
         </button>
       </div>
     </nav>
