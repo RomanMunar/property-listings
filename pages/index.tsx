0d9a5c88b33421cccaa83afd485eb75e42ac1fd8
romanmunar <roman.munar1@gmail.com>
2020-12-29 20:23:33 +0800

    improved grammar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { Layout } from "../components"
-import Image from "next/image"
 const Home = () => {
   return (
     <div className="w-full font-serif min-h-page">
       <div className="relative">
         <div
           style={{ backgroundImage: 'url("/homepace_image.jpg")' }}
-          className="bg-no-repeat bg-cover w-fullbg-center h-52"
+          className="w-full bg-center bg-no-repeat bg-cover h-52"
         ></div>
         <div className="absolute top-0 w-full h-full bg-gray-700 bg-opacity-50"></div>
-        <div className="absolute top-0 z-10 text-white">
-          <span className="text-amber-400">John Hitchkins</span> is a Real Estate Partners is #1 in the Santa Barbara
-          MLS for transactions with over $1 Billion in sales.
+        <div className="absolute z-10 px-10 text-white top-1/4">
+          <span className="block text-3xl font-bold text-amber-500">John Hitchkins</span>
+          <span className="block ml-5 text-xl">
+            #1 in the Santa Barbara MLS for transactions with over $1 Billion in sales.
+          </span>
         </div>
       </div>
     </div>
