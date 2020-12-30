37329a2d8fd56caec31cdcf57e6e44116ce3ef9b
romanmunar <roman.munar1@gmail.com>
2020-12-30 22:10:14 +0800

    added exports for property grid and card

--- components/core/index.ts
+++ components/core/index.ts
@@ -1,2 +1,4 @@
 export { default as Head } from "./head"
 export { default as Navbar } from "./navbar"
+export { default as PropertyGrid } from "./propertyGrid"
+export { default as PropertyCard } from "./propertyCard"
