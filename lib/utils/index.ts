821eb7f9876ae665b53f9a70828ce532acb13483
romanmunar <roman.munar1@gmail.com>
2021-01-01 15:10:05 +0800

    added useMedia hook

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as useMedia } from "./useMedia"
 export { default as enableScroll } from "./enableScroll"
 export { default as disableScroll } from "./disableScroll"
 export const isClient = typeof window === "object"
--- lib/utils/useMedia.ts
+++ lib/utils/useMedia.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import { isClient } from "."
 
+/* snippet from
+  https:streamich.github.io/react-use/?path=/story/sensors-usemedia--docs
+ */
 const useMedia = (query: string, defaultState: boolean = false) => {
   const [state, setState] = useState(isClient ? () => window.matchMedia(query).matches : defaultState)
 
