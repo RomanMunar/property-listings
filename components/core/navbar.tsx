c5bb9f1e029ca70f81b68ba2f24b440214b7ada9
romanmunar <roman.munar1@gmail.com>
2021-01-04 10:40:09 +0800

    navbar now uses navitem on mobile screen

--- components/core/navbar.tsx
+++ components/core/navbar.tsx
@@ -48,10 +48,9 @@ const navbar = () => {
           />
           <nav className="fixed bottom-0 z-50 w-full duration-300 transform h-1/2">
             <ul className="flex flex-col items-center justify-around w-full h-full py-10 space-y-4 text-white bg-gray-700 rounded-tl-2xl rounded-tr-2xl top-1/2">
-              <li className="px-4 py-1 font-bold border-transparentborder-b-2 border-amber-500">Home</li>
-              <li className="px-4 py-1 border-b-2 border-transparent">Listings</li>
-              <li className="px-4 py-1 border-b-2 border-transparent">Testimonials</li>
-              <li className="px-4 py-1 border-b-2 border-transparent">Contact</li>
+              {navbarRoutes.map((n) => (
+                <NavItem active={asPath === n.dest} key={`route-${n.route}`} route={n.route} dest={n.dest} />
+              ))}
             </ul>
           </nav>
         </>
